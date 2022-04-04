82791991dc4f4714c6ee59d328cfc6bdd9b8a290
Pyeon9 <letter1006@naver.com>
2022-04-04 17:18:28 +0900

    solve problem using stable_sort

--- Baekjoon/C++/1. Step/12. sort/10814/sort_member.cpp
+++ Baekjoon/C++/1. Step/12. sort/10814/sort_member.cpp
@@ -5,11 +5,8 @@
 
 using namespace std;
 
-bool compare(pair<int, pair<int, string>> a, pair<int, pair<int, string>> b){
-    if (a.second.first == b.second.first){        // 나이가 같으면
-        return a.first < b.first;                 // 가입순으로. 인덱스 오름차순
-    }
-    else    return a.second.first < b.second.first;     // 나이 증가 순
+bool compare(pair<int, string> a, pair<int, string> b){
+    return a.first < b.first;     // 나이 증가 순   나이 같은 경우는 stable_sort에 의해 정렬 전 순서대로 정렬됨
 }
 
 int main(){
@@ -17,18 +14,18 @@ int main(){
 
     int n;
     cin >> n;
-    vector<pair<int, pair<int, string>>> v;    // <인덱스, <나이, 이름>>
+    vector<pair<int, string>> v;    // <인덱스, <나이, 이름>>
     for (int i = 0; i < n; i++){
         int age;
         string name;
         cin >> age >> name;
-        v.push_back(make_pair(i, make_pair(age, name)));
+        v.push_back(make_pair(age, name));
     }
 
-    sort(v.begin(), v.end(), compare);
+    stable_sort(v.begin(), v.end(), compare);
 
     for (int i = 0; i < n; i++){
-        cout << v[i].second.first << ' ' << v[i].second.second << '\n';
+        cout << v[i].first << ' ' << v[i].second << '\n';
     }
 
     return 0;
